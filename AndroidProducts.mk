b919bd1c252a767acd0a5db3ab4c229e4de9a404
gio1407 <100862648+gio1407@users.noreply.github.com>
2022-07-17 20:16:08 +0200

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_i11_Pro.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_i11_Pro-user \
+    omni_i11_Pro-userdebug \
+    omni_i11_Pro-eng
